efabb544cd43fa8cc44a8fe2827de893a2f3188c
IND-INF <oinformacjy2000@yandex.ru>
2026-05-15 22:19:26 +0300

    Add files via upload

---
+++ sequence_exceptions.h
@@ -0,0 +1,48 @@
+#ifndef _SEQUENCE_EXEPTIONS_H_
+#define _SEQUENCE_EXEPTIONS_H_
+
+#include <cstddef>
+#include <exception>
+#include <string>
+
+class IndexOutOfRange : public std::exception{
+    private:
+    
+        std::size_t enteredIndex;
+        std::size_t minimalLimit;
+        std::size_t maximalLimit;
+
+        std::string message;
+
+    public:
+
+        IndexOutOfRange(std::size_t entered, std::size_t minimal, std::size_t maximal);
+
+        const char* what() const noexcept override;
+        std::size_t GetEnteredIndex() const;
+        std::size_t GetMinimalLimit() const;
+        std::size_t GetMaximalLimit() const;
+
+};
+
+class EnumeratorCtorError : public std::exception{
+    public:
+        const char* what() const noexcept override;
+};
+
+class ListCtorError : public std::exception{
+    public:
+        const char* what() const noexcept override;
+};
+
+class EmptySequence : public std::exception{
+    public:
+        const char* what() const noexcept override;
+};
+
+class BadCollectionCast : public std::exception{
+    public:
+        const char* what() const noexcept override;
+};
+
+#endif //_SEQUENCE_EXEPTIONS_H_
